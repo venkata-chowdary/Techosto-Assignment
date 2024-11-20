e5604aa463997c00d1fae1c80c2404947bd47682
Chowdary Immanni <chowdaryimmanni@gmail.com>
2024-11-20 13:11:01 +0530

    completed assignment but need to host everything

---
+++ frontend/src/App.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'; // Importing axios
+import { Row, Col } from 'antd';
+import ProfileCard from './Components/ProfileCard';
+import Loader from './Components/Loader';
+
+function App() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    setLoading(true)
+    axios
+      .get('http://localhost:5000/api/get-user-details')
+      .then((response) => {
+        setUsers(response.data);
+        setLoading(false)
+      })
+      .catch((err) => console.error('Error fetching user details:', err));
+  }, []);
+
+  return (
+    <div>
+      {loading ? <Loader /> :
+        <Row >
+          {users.map((user) => (
+            <Col key={user._id} xs={24} sm={12} lg={6}>
+              <ProfileCard user={user} setUsers={setUsers} />
+            </Col>
+          ))}
+        </Row>}
+
+    </div>
+  );
+}
+
+export default App;
